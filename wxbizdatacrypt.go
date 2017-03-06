d4b185b9a6687db8c8faeef7f38fbbb173352d4f
施凯伦 <kshi@linkedin.com>
2017-03-06 10:08:07 +0800

    update pkcs#7unpad

--- wxbizdatacrypt.go
+++ wxbizdatacrypt.go
@@ -6,13 +6,18 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"errors"
-	"strings"
 )
 
-var ErrorAppIDNotMatch = errors.New("app id not match")
+var (
+	ErrAppIDNotMatch       = errors.New("app id not match")
+	ErrInvalidBlockSize    = errors.New("invalid block size")
+	ErrInvalidPKCS7Data    = errors.New("invalid PKCS7 data")
+	ErrInvalidPKCS7Padding = errors.New("invalid padding on input")
+)
 
 type UserInfo struct {
 	OpenID    string `json:"openId"`
+	UnionID   string `json:"unionId"`
 	NickName  string `json:"nickName"`
 	Gender    int    `json:"gender"`
 	City      string `json:"city"`
@@ -37,6 +42,27 @@ func NewWXBizDataCrypt(appID, sessionKey string) *WXBizDataCrypt {
 	}
 }
 
+// pkcs7Unpad returns slice of the original data without padding
+func pkcs7Unpad(data []byte, blockSize int) ([]byte, error) {
+	if blockSize <= 0 {
+		return nil, ErrInvalidBlockSize
+	}
+	if len(data)%blockSize != 0 || len(data) == 0 {
+		return nil, ErrInvalidPKCS7Data
+	}
+	c := data[len(data)-1]
+	n := int(c)
+	if n == 0 || n > len(data) {
+		return nil, ErrInvalidPKCS7Padding
+	}
+	for i := 0; i < n; i++ {
+		if data[len(data)-n+i] != c {
+			return nil, ErrInvalidPKCS7Padding
+		}
+	}
+	return data[:len(data)-n], nil
+}
+
 func (w *WXBizDataCrypt) Decrypt(encryptedData, iv string) (*UserInfo, error) {
 	aesKey, err := base64.StdEncoding.DecodeString(w.sessionKey)
 	if err != nil {
@@ -56,15 +82,17 @@ func (w *WXBizDataCrypt) Decrypt(encryptedData, iv string) (*UserInfo, error) {
 	}
 	mode := cipher.NewCBCDecrypter(block, ivBytes)
 	mode.CryptBlocks(cipherText, cipherText)
-	cipherText = []byte(strings.Replace(string(cipherText), "\f", "", -1))
-	cipherText = []byte(strings.Replace(string(cipherText), "\a", "", -1))
+	cipherText, err = pkcs7Unpad(cipherText, block.BlockSize())
+	if err != nil {
+		return nil, err
+	}
 	var userInfo UserInfo
 	err = json.Unmarshal(cipherText, &userInfo)
 	if err != nil {
 		return nil, err
 	}
 	if userInfo.Watermark.AppID != w.appID {
-		return nil, ErrorAppIDNotMatch
+		return nil, ErrAppIDNotMatch
 	}
 	return &userInfo, nil
 }
